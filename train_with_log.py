3991134481fc77d060328dfab64599d7bd32b8eb
aminpaydar <mapsgp920@elenoon.ir>
2019-05-10 12:36:15 +0430

    adds upload to drive

--- train_with_log.py
+++ train_with_log.py
@@ -1,103 +1,102 @@
-# import necessary modules from keras
 from keras.layers import Dense
 from keras.models import Sequential
-
-# creates a generic neural network architecture
-model = Sequential()
-
-# hidden layer takes a pre-processed frame as input, and has 200 units
-model.add(Dense(units=200,input_dim=80*80, activation='relu', kernel_initializer='glorot_uniform'))
-
-# output layer
-model.add(Dense(units=1, activation='sigmoid', kernel_initializer='RandomNormal'))
-
-# compile the model using traditional Machine Learning losses and optimizers
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
+from keras import callbacks
 import numpy as np
 import gym
+from easy_tf_log import tflog
+from datetime import datetime
+import os
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
 
-# gym initialization
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
+
+UP_ACTION = 2
+DOWN_ACTION = 3
+gamma = 0.99
+x_train, y_train, rewards = [],[],[]
+reward_sum = 0
+episode_nb = 0
+resume = True
+running_reward = None
+epochs_before_saving = 100
+log_dir = './log' + datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
+pre_trained_file = "my_model_weights20190507-214912.h5"
+
+def preprocess(input):
+  input = input[35:195] # crop
+  input = input[::2, ::2, 0] # downsample by factor of 2
+  input[input == 144] = 0 # erase background (background type 1)
+  input[input == 109] = 0 # erase background (background type 2)
+  input[input != 0] = 1 # everything else (paddles, ball) just set to 1
+  return input.astype(np.float).ravel()
+
+def discount_rewards(r, gamma):
+  r = np.array(r)
+  discounted_r = np.zeros_like(r)
+  running_add = 0
+  for t in reversed(range(0, r.size)):
+    if r[t] != 0: running_add = 0 # if the game ended (in Pong), reset the reward sum
+    running_add = running_add * gamma + r[t] # the point here is to use Horner's method to compute those rewards efficiently
+    discounted_r[t] = running_add
+  discounted_r -= np.mean(discounted_r) #normalizing the result
+  discounted_r /= np.std(discounted_r) #idem
+  return discounted_r
+
+model = Sequential()
+model.add(Dense(units=200,input_dim=80*80, activation='relu', kernel_initializer='glorot_uniform'))
+model.add(Dense(units=1, activation='sigmoid', kernel_initializer='RandomNormal'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
 env = gym.make("Pong-v0")
 observation = env.reset()
 prev_input = None
 
-# Macros
-UP_ACTION = 2
-DOWN_ACTION = 3
-
-# Hyperparameters
-gamma = 0.99
-
-# initialization of variables used in the main loop
-x_train, y_train, rewards = [],[],[]
-reward_sum = 0
-episode_nb = 0
-
-from easy_tf_log import tflog
-from datetime import datetime
-from keras import callbacks
-import os
-
-# initialize variables
-resume = True
-running_reward = None
-epochs_before_saving = 10
-log_dir = './log' + datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
-
-# load pre-trained model if exist
-if (resume and os.path.isfile('my_model_weights.h5')):
+if (resume and os.path.isfile(pre_trained_file)):
     print("loading previous weights")
-    model.load_weights('my_model_weights.h5')
+    model.load_weights(pre_trained_file)
 
-# add a callback tensorboard object to visualize learning
 tbCallBack = callbacks.TensorBoard(log_dir=log_dir, histogram_freq=0,
                                    write_graph=True, write_images=True)
 
-from karpathy import prepro, discount_rewards
-
-# main loop
 while (True):
-
-    # preprocess the observation, set input as difference between images
-    cur_input = prepro(observation)
+    cur_input = preprocess(observation)
     x = cur_input - prev_input if prev_input is not None else np.zeros(80 * 80)
     prev_input = cur_input
 
-    # forward the policy network and sample action according to the proba distribution
     proba = model.predict(np.expand_dims(x, axis=1).T)
     action = UP_ACTION if np.random.uniform() < proba else DOWN_ACTION
     y = 1 if action == 2 else 0  # 0 and 1 are our labels
 
-    # log the input and label to train later
     x_train.append(x)
     y_train.append(y)
 
-    # do one step in our environment
     observation, reward, done, info = env.step(action)
     rewards.append(reward)
     reward_sum += reward
 
-    # end of an episode
     if done:
         print('At the end of episode', episode_nb, 'the total reward was :', reward_sum)
-
-        # increment episode number
         episode_nb += 1
-
-        # training
         model.fit(x=np.vstack(x_train), y=np.vstack(y_train), verbose=1, callbacks=[tbCallBack],
                   sample_weight=discount_rewards(rewards, gamma))
 
-        # Saving the weights used by our model
         if episode_nb % epochs_before_saving == 0:
-            model.save_weights('my_model_weights' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.h5')
+            model_file = 'my_model_weights' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.h5'
+            model.save_weights()
+            uploaded = drive.CreateFile({'title': 'my_model_weights' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.h5'})
+            uploaded.SetContentFile(model_file)
+            uploaded.Upload()
+            print('Uploaded file with ID {}'.format(uploaded.get('id')))
 
-        # Log the reward
         running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
         tflog('running_reward', running_reward, custom_dir=log_dir)
 
-        # Reinitialization
         x_train, y_train, rewards = [], [], []
         observation = env.reset()
         reward_sum = 0
